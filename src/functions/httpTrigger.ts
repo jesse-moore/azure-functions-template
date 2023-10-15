c9273cec0e44a034b32e4238290fd395fc223a41
Jesse Moore <moore.jesse@gmail.com>
2023-10-15 08:45:04 -0500

    Added testing

--- src/functions/httpTrigger.ts
+++ src/functions/httpTrigger.ts
@@ -2,7 +2,6 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 
 export async function httpTrigger(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     const name = request.query.get('name') || await request.text() || 'world';
-
     return { body: `Hello, ${name}!` };
 };
 
